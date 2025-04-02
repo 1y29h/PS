2e47084a87c4d8967deb16e72f82bd8caf0e21ef
yeon <rladusgml129@naver.com>
2025-04-02 14:38:12 +0900

    Solve [ BOJ 02743 / JAVA ]

---
+++ baekjoon/bronze/p02743.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class p02743 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String [] sent = sc.nextLine().split("");
+        System.out.println(sent.length);
+
+    }
+}
