16d7edb12641af5f8d3c67b25577fe8dc04ef618
yeon <rladusgml129@naver.com>
2025-04-09 20:18:21 +0900

    Solve [ BOJ 05341 / JAVA ]

--- baekjoon/bronze/p05341.java
+++ baekjoon/bronze/p05341.java
@@ -3,23 +3,7 @@ import java.io.*;
 public class p05341 {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while(true){
-
-            int top = Integer.parseInt(br.readLine());
-            int blocks = 0;
-
-            if(top ==0){
-                break;
-            }
-
-            for (int i=top; i>=1; i--){
-                blocks += i;
-            }
-
-            System.out.println(blocks);
-        }
 
     }
 }
