6ddf4738cad2850ecacbeff1a073a75c793027f1
yeon <rladusgml129@naver.com>
2025-04-19 17:09:08 +0900

    Solve [ BOJ 01598 / JAVA ]

---
+++ baekjoon/bronze/p01598.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class p01598 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nums = br.readLine().split(" ");
+        int n1 = Integer.parseInt(nums[0]);
+        int n2 = Integer.parseInt(nums[1]);
+        int[] x = new int[2];
+        int[] y = new int[2];
+
+        if (n1 % 4 == 0) {
+            x[0] = n1 / 4;
+            y[0] = 4;
+        } else {
+            x[0] = n1 / 4 + 1;
+            y[0] = n1 % 4;
+        }
+
+        if (n2 % 4 == 0) {
+            x[1] = n2 / 4;
+            y[1] = 4;
+        } else {
+            x[1] = n2 / 4 + 1;
+            y[1] = n2 % 4;
+        }
+
+        System.out.println(Math.abs(x[0] - x[1]) + Math.abs(y[0] - y[1]));
+
+    }
+}
