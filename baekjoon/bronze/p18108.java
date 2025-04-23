db7f6ed73f9d35964a60c255d6534d08242604ed
yeon <rladusgml129@naver.com>
2025-04-23 11:26:12 +0900

    Solve [ BOJ 18108 / JAVA ]

---
+++ baekjoon/bronze/p18108.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class p18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(y-543);
+
+    }
+}
