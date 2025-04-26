eb8e2c0f7e6de5c391e8ae718d2d8ecc68c426fc
yeon <rladusgml129@naver.com>
2025-04-26 15:20:22 +0900

    Solve [ BOJ 10699 / JAVA ]

---
+++ baekjoon/bronze/p10699.java
@@ -0,0 +1,10 @@
+import java.text.*;
+import java.util.*;
+
+public class p10699 {
+    public static void main(String[] args) {
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sd.format(new Date()));
+
+    }
+}
