13b54d518a635badbadfb19da1ade0265b25644b
yeon <rladusgml129@naver.com>
2025-04-30 12:38:06 +0900

    Solve [ BOJ 15740 / JAVA ]

---
+++ baekjoon/bronze/p15740.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+
+public class p15740 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] str = br.readLine().split(" ");
+        BigInteger A = new BigInteger(str[0]);
+        BigInteger B = new BigInteger(str[1]);
+
+        System.out.println(A.add(B));
+
+    }
+}
