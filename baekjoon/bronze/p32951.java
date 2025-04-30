fb4bdb2effd2bc25e3f0be5f9b2b7e79cfafa19b
yeon <rladusgml129@naver.com>
2025-04-30 17:20:57 +0900

    Solve [ BOJ 32951 / JAVA ]

---
+++ baekjoon/bronze/p32951.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class p32951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N-2024);
+
+    }
+}
