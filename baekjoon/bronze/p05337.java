68f4072fbecda44feec2f92890216375590c2d77
yeon <rladusgml129@naver.com>
2025-05-21 20:37:46 +0900

    Solve [ BOJ 05337 / JAVA ]

---
+++ baekjoon/bronze/p05337.java
@@ -0,0 +1,7 @@
+public class p05337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
