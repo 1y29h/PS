c686981099068e8cbe737aecc1389dd9c2cd1e0d
yeon <rladusgml129@naver.com>
2025-05-25 22:37:13 +0900

    Solve [ BOJ 32458 / JAVA ]

---
+++ baekjoon/bronze/p32458.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class p32458 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        float x = Float.parseFloat(br.readLine());
+        System.out.println((int)x);
+    }
+}
