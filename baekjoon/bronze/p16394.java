b223685e110c790a0e71d3179f1fb906ca7269c2
Yeon <1y29h0.0@gmail.com>
2025-11-22 05:53:19 +0900

    Solve [ BOJ 16394 / JAVA ]

---
+++ baekjoon/bronze/p16394.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class p16394 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N-1946);
+
+    }
+}
