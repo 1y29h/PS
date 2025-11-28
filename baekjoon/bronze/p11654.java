8cc10a4ffaa05426b3d8eb01d0eeeb990ae0ebbe
Yeon <1y29h0.0@gmail.com>
2025-11-28 17:58:54 +0900

    Solve [ BOJ 11654 / JAVA ]

---
+++ baekjoon/bronze/p11654.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class p11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = br.readLine();
+        System.out.println(n.hashCode());
+
+    }
+}
