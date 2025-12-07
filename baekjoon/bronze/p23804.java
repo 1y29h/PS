08e3a113baf80be8c28e0a4ce9b6168c77051c74
Yeon <1y29h0.0@gmail.com>
2025-12-07 18:59:42 +0900

    Solve [ BOJ 23804 / JAVA ]

---
+++ baekjoon/bronze/p23804.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class p23804 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println("@@@@@".repeat(N));
+        }
+        for (int i = 1; i <= N; i++) {
+            System.out.println("@".repeat(N));
+            System.out.println("@".repeat(N));
+            System.out.println("@".repeat(N));
+        }
+        for (int i = 1; i <= N; i++) {
+            System.out.println("@@@@@".repeat(N));
+        }
+
+    }
+}
