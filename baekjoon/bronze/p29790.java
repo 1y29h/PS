e09dbe7ceda3b1f0fe3ba35ff89dd5e1b26b3579
Yeon <1y29h0.0@gmail.com>
2025-12-13 02:04:17 +0900

    Solve [ BOJ 29790 / JAVA ]

---
+++ baekjoon/bronze/p29790.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class p29790 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int U = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        if (N >= 1000) {
+            if (U >= 8000 || L >= 260) {
+                System.out.println("Very Good");
+            }
+            else {
+                System.out.println("Good");
+            }
+        }
+        else {
+            System.out.println("Bad");
+        }
+
+    }
+}
+
+
+
+
+
+
+
