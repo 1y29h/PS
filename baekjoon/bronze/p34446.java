0d04157147d300f4b98f41b007382014cc298b4c
Yeon <1y29h0.0@gmail.com>
2025-12-13 02:03:33 +0900

    Solve [ BOJ 34446 / JAVA ]

---
+++ baekjoon/bronze/p34446.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class p34446 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+
+        System.out.print(m*2);
+
+    }
+}
