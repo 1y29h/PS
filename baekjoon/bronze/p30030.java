4c544859adf1c1c6ea93161bf820869845041b16
Yeon <1y29h0.0@gmail.com>
2026-01-12 09:22:46 +0900

    Solve [ BOJ 30030 / JAVA ]

---
+++ baekjoon/bronze/p30030.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class p30030 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(B/11*10);
+
+    }
+}
