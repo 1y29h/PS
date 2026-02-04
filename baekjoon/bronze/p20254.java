28fd135903f2c6bedbfd0f743264dd738ecfeafe
Yeon <1y29h0.0@gmail.com>
2026-02-04 19:13:38 +0900

    Solve [ BOJ 20254 / JAVA ]

---
+++ baekjoon/bronze/p20254.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class p20254 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int ur = Integer.parseInt(st.nextToken());
+        int tr = Integer.parseInt(st.nextToken());
+        int uo = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+
+        System.out.print(56*ur + 24*tr + 14*uo + 6*to);
+    }
+}
